ed1fd08e26a77e20d7f9e29adaa8496a9a08a1ef
ykn <244115113@qq.com>
2022-03-28 00:18:31 +0800

    build

--- vue3-admin/dist/assets/dashboard.61fdcd6c.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./index.5aa7076e.js";import{d as t,a as n,o as s,t as o}from"./vendor.4870a2ae.js";const a=t({name:"Dashboard",setup(){return{}}}),r=o("h1",null,"\u4EEA\u8868\u76D8",-1),c=o("i",{class:"iconfont icon-configure"},null,-1),_=[r,c];function d(i,u,l,p,f,m){return s(),n("div",null,_)}var x=e(a,[["render",d]]);export{x as default};
-//# sourceMappingURL=dashboard.61fdcd6c.js.map
