106f331ff57690dc290b541863f95a619763ea7c
ykn <244115113@qq.com>
2022-03-30 00:05:16 +0800

    build

--- vue3-admin/dist/assets/404.b41eb5a0.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./index.53ed169a.js";import{b as r,o}from"./vendor.be1891b2.js";const c={};function t(n,_){return o(),r("div",null,"404")}var f=e(c,[["render",t]]);export{f as default};
-//# sourceMappingURL=404.b41eb5a0.js.map
